c93e92635cb638fec380164920bdb809abc2c45a
Cyril Arinze <ejicyrilarinze@gmail.com>
2023-01-07 01:41:43 +0100

    refactor(Navbar): move navbar to new file

--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -2,60 +2,23 @@ import React from "react";
 
 import customStyles from "./Layout.module.css";
 import Header from "./Header";
-import { NavLink } from "react-router-dom";
-import { Logo } from "./Logo";
 import { useSelector } from "react-redux";
 import { navbarState } from "../../store/redux-slices/UI-slice";
-
-const Navbar = () => {
-  const activeNavLinkHandler = ({ isActive }) => {
-    return isActive
-      ? "bg-active-link px-2 w-full inline-block"
-      : "px-2 inline-block";
-  };
-  return (
-    <nav
-      className={`bg-primary-color text-white text-xl py-3 font-Montserrat h-fit w-fit border border-white  ${customStyles.navbar} absolute top-[90px] left-[5%] sm:relative sm:flex sm:left-0 sm:top-0 sm:flex-col sm:px-6 sm:py-8 sm:h-full sm:space-y-8 sm:border-none`}
-    >
-      <div className="hidden sm:flex justify-center ">
-        <Logo />
-      </div>
-
-      <ul className=" space-y-4">
-        <li>
-          <NavLink to="/" className={activeNavLinkHandler}>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/New-Releases" className={activeNavLinkHandler}>
-            New Releases
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Artsboardz" className={activeNavLinkHandler}>
-            Artsboardz
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Leaderboardz" className={activeNavLinkHandler}>
-            Leaderboardz
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+import Navbar from "../Navigation/Navbar";
 
 const Layout = ({ children }) => {
-  const isNavShown = useSelector(navbarState);
+  const navBarState = useSelector(navbarState);
+  const isNavShown = window.innerWidth > 680 ? true : navBarState;
+
   return (
     <div
-      className={`min-h-screen ${customStyles.layout} max-w-7xl mx-auto relative`}
+      className={`h-screen ${customStyles.layout} max-w-7xl mx-auto relative`}
     >
       <Header />
       {isNavShown && <Navbar />}
-      <main className={`bg-red-900 min-h-full ${customStyles.main}`}>
+      <main
+        className={`bg-light-purple  min-h-full ${customStyles.main} overflow-y-auto`}
+      >
         {children}
       </main>
     </div>
---
+++ src/components/Navigation/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import customStyles from "../Layouts/Layout.module.css";
+import { NavLink } from "react-router-dom";
+import { Logo } from "../Layouts/Logo";
+import { useDispatch } from "react-redux";
+import { UIAction } from "../../store/redux-slices/UI-slice";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const activeNavLinkHandler = ({ isActive }) => {
+    return isActive
+      ? "bg-active-link px-2 w-full inline-block"
+      : "px-2 inline-block";
+  };
+
+  const toggleNavbarHandler = () => {
+    dispatch(UIAction.toggleNavbar());
+  };
+
+  return (
+    <nav
+      className={`bg-primary-color text-white text-xl py-3 font-Montserrat h-fit w-fit border border-white  ${customStyles.navbar} absolute top-[90px] left-[5%] sm:relative sm:flex sm:left-0 sm:top-0 sm:flex-col sm:px-6 sm:py-8 sm:h-full sm:space-y-8 sm:border-none`}
+    >
+      <div className="hidden sm:flex justify-center ">
+        <Logo />
+      </div>
+
+      <ul className=" space-y-4">
+        <li onClick={toggleNavbarHandler}>
+          <NavLink to="/" className={activeNavLinkHandler}>
+            Home
+          </NavLink>
+        </li>
+        <li onClick={toggleNavbarHandler}>
+          <NavLink to="/New-Releases" className={activeNavLinkHandler}>
+            New Releases
+          </NavLink>
+        </li>
+        <li onClick={toggleNavbarHandler}>
+          <NavLink to="/Artsboardz" className={activeNavLinkHandler}>
+            Artsboardz
+          </NavLink>
+        </li>
+        <li onClick={toggleNavbarHandler}>
+          <NavLink to="/Leaderboardz" className={activeNavLinkHandler}>
+            Leaderboardz
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
